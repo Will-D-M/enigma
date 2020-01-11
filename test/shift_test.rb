f024362332a9fc82104508cd14c50d73ebc88887
William Meighan <williamdmeighan@gmail.com>
2020-01-11 13:42:29 -0700

    Make testing more robust to account for helper methods that were input

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,8 +3,37 @@ require_relative '../lib/shift'
 
 class ShiftTest < Minitest::Test
 
+  def test_date_sqaured
+    assert_equal "1672401025", Shift.date_squared("02715", "040895")
+  end
+
+  def test_initial_offset
+    assert_equal ["1", "0", "2", "5"], Shift.initial_offset("02715", "040895")
+  end
+
+  def test_initial_key_array
+    assert_equal ["02", "27", "71", "15"], Shift.initial_key("02715", "040895")
+  end
+
+  def test_a_index
+    assert_equal 3, Shift.a_index("02715", "040895")
+  end
+
+  def test_b_index
+    assert_equal 27, Shift.b_index("02715", "040895")
+  end
+
+  def test_c_index
+    assert_equal 73, Shift.c_index("02715", "040895")
+  end
+
+  def test_d_index
+    assert_equal 20, Shift.d_index("02715", "040895")
+  end
+
   def test_it_can_find_final_shift
-    assert_equal 3, Shift.final_shift("02715", "040895")[0]
+    expected = {"a"=>3, "b"=>27, "c"=>73, "d"=>20}
+    assert_equal expected, Shift.final_shift("02715", "040895")
   end
 
 end
