11fea91a7eed2271b75d14709fd52fe6cf65319f
William Meighan <williamdmeighan@gmail.com>
2020-01-10 17:47:42 -0700

    Add test to confirm it can return random number string

--- test/key_test.rb
+++ test/key_test.rb
@@ -11,4 +11,9 @@ class KeyTest < Minitest::Test
     assert_instance_of Key, @key
   end
 
+  def test_it_can_generate_random_number_string
+    assert_instance_of String, @key.random_number_string
+    assert_equal 5, @key.random_number_string.length
+  end
+
 end
