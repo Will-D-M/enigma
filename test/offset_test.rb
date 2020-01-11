578dae9634a0476f00e93f085e9951fbb080e5e6
William Meighan <williamdmeighan@gmail.com>
2020-01-10 18:23:21 -0700

    Implementing stub since date is going to change every day

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -4,6 +4,7 @@ require_relative '../lib/offset'
 class OffsetTest < Minitest::Test
 
   def test_it_can_generate_current_date
+    Offset.stubs(:current_date).returns("100120")
     assert_equal "100120", Offset.current_date
   end
 
