9770a634b650ca91beba0c88e22a7a12ff34d010
William Meighan <williamdmeighan@gmail.com>
2020-01-13 13:52:42 -0700

    Make testing more robust for Key class

--- test/key_test.rb
+++ test/key_test.rb
@@ -6,6 +6,7 @@ class KeyTest < Minitest::Test
   def test_it_can_generate_random_number_string
     assert_instance_of String, Key.random_number_string
     assert_equal 5, Key.random_number_string.length
+    assert_equal true, Key.random_number_string.to_i > 0 && Key.random_number_string.to_i < 100000
   end
 
 end
