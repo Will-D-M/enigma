647dc5016bf5fd45b94fc3632c1b3339ced168da
William Meighan <williamdmeighan@gmail.com>
2020-01-14 19:04:17 -0700

    Adjust testing for condensed shift_number method

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -3,32 +3,27 @@ require_relative '../lib/shift'
 
 class ShiftTest < Minitest::Test
 
+  def setup
+    @shift = Shift.new("02715", "040895")
+  end
+
   def test_date_sqaured
-    assert_equal "1672401025", Shift.date_squared("02715", "040895")
+    assert_equal "1672401025", @shift.date_squared
   end
 
   def test_initial_offset
-    assert_equal ["1", "0", "2", "5"], Shift.initial_offset("02715", "040895")
+    assert_equal ["1", "0", "2", "5"], @shift.initial_offset
   end
 
   def test_initial_key_array
-    assert_equal ["02", "27", "71", "15"], Shift.initial_key("02715", "040895")
+    assert_equal ["02", "27", "71", "15"], @shift.initial_key
   end
 
-  def test_a_index
-    assert_equal 3, Shift.a_index("02715", "040895")
-  end
-
-  def test_b_index
-    assert_equal 27, Shift.b_index("02715", "040895")
-  end
-
-  def test_c_index
-    assert_equal 73, Shift.c_index("02715", "040895")
-  end
-
-  def test_d_index
-    assert_equal 20, Shift.d_index("02715", "040895")
+  def test_shift_number
+    assert_equal 3, @shift.shift_number(0)
+    assert_equal 27, @shift.shift_number(1)
+    assert_equal 73, @shift.shift_number(2)
+    assert_equal 20, @shift.shift_number(3)
   end
 
 end
