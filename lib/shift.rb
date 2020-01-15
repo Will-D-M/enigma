cef79eb7e8f1b8719800f13c12b8777577f960c6
William Meighan <williamdmeighan@gmail.com>
2020-01-14 19:03:39 -0700

    Initialize so we can use instance methods instead of class methods. Also condensed seperate index methods into one shift_number method.

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,14 +1,21 @@
 class Shift
 
-  def self.date_squared(key, date)
-    ((date.to_i) ** 2).to_s
+  attr_reader :key, :date
+
+  def initialize(key, date)
+    @key = key
+    @date = date
   end
 
-  def self.initial_offset(key, date)
-    date_squared(key, date).chars.values_at(-4, -3, -2, -1)
+  def date_squared
+    ((@date.to_i) ** 2).to_s
   end
 
-  def self.initial_key(key, date)
+  def initial_offset
+    date_squared.chars.values_at(-4, -3, -2, -1)
+  end
+
+  def initial_key
     [
       key.chars.values_at(0, 1).join,
       key.chars.values_at(1, 2).join,
@@ -17,20 +24,7 @@ class Shift
     ]
   end
 
-  def self.a_index(key, date)
-    initial_key(key, date)[0].to_i + initial_offset(key, date)[0].to_i
+  def shift_number(index)
+    initial_key[index].to_i + initial_offset[index].to_i
   end
-
-  def self.b_index(key, date)
-    initial_key(key, date)[1].to_i + initial_offset(key, date)[1].to_i
-  end
-
-  def self.c_index(key, date)
-    initial_key(key, date)[2].to_i + initial_offset(key, date)[2].to_i
-  end
-
-  def self.d_index(key, date)
-    initial_key(key, date)[3].to_i + initial_offset(key, date)[3].to_i
-  end
-
 end
